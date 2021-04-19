f63fc2cf8992d211580756b420fa2ee730ce4165
Bianca Chien <cocoamacaron@gmail.com>
2021-04-19 18:28:36 +0800

    'init'

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import requests from './requests'
+import Row from './Row'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+    </div>
+  );
+}
+
+export default App;
