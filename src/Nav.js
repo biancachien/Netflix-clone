17526088a429db853b8b1568d049ddf55c68915d
Bianca Chien <cocoamacaron@gmail.com>
2021-04-20 18:27:51 +0800

    update

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,8 @@
 import React from "react";
+import "./Nav.css";
 
 function Nav() {
+//   useEffect(() => {}, []);
   return (
     <div className="nav">
       <img
@@ -8,8 +10,11 @@ function Nav() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1024px-Netflix_2015_logo.svg.png"
         alt="Netflix Logo"
       />
-      <img className="nav-avatar" 
-      src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" />
+      <img
+        className="nav-avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Avatar"
+      />
     </div>
   );
 }
